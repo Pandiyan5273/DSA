1726455488e943dde1b6015aa0e4814ebbb2c818
Pandiyan P <pandiyanpalani37@gmail.com>
2025-04-01 10:16:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Two Sets/union-of-two-sets.py
@@ -0,0 +1,18 @@
+
+a = set([int(x) for x in input().strip().split()])
+b = set([int(x) for x in input().strip().split()])
+
+########### Write your code below ###############
+a=list(a)
+b=list(b)
+c=a+b
+st =set(c)
+print(len(st))
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
