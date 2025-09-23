892ee43648eb191fba6eb5979e4e2fdedb62ffdc
Pandiyan P <pandiyanpalani37@gmail.com>
2025-09-23 13:31:46 +0530

    Time: 14 ms (78.80%) | Memory: 17.9 MB (54.35%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        a=Counter(ransomNote)
+        b=Counter(magazine)
+        for i,j in a.items():
+            if b[i]<j:
+                return False
+        return True         
