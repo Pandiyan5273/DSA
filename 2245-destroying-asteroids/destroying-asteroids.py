86dbbe6f65ed0456b844dacc955f386759b30ede
Pandiyan P <pandiyanpalani37@gmail.com>
2026-05-31 07:53:15 +0530

    Time: 79 ms (40.88%) | Memory: 33.9 MB (53.36%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -0,0 +1,8 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+        for m in asteroids:
+            if mass<m:
+                return False
+            mass+=m
+        return True
